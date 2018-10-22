a2aaa3c2d1bb3c9159b1ade543c19977782d541e
clach <clach@seas.upenn.edu>
2018-10-22 13:48:00 -0400

    fixed clicking on meshes, added UI

---
+++ MuseumMR/Assets/ToggleObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleObjects : MonoBehaviour {
+
+    public GameObject model1;
+    public GameObject model2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSelect()
+    {
+        Debug.Log("MESSAGE RECEIVED!");
+
+        // toggle object views
+        model1.SetActive(!model1.activeSelf);
+        model2.SetActive(!model2.activeSelf);
+        //this.gameObject.SetActive(!this.gameObject.activeSelf);
+    }
+}
