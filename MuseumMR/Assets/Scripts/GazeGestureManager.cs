92745bbc8f3fc40bee819a93a69a3686a52d88b8
clach <clach@seas.upenn.edu>
2018-10-24 13:48:33 -0400

    fixed airtap recognition to use HoloToolkit

--- MuseumMR/Assets/Scripts/GazeGestureManager.cs
+++ MuseumMR/Assets/Scripts/GazeGestureManager.cs
@@ -5,9 +5,6 @@ public class GazeGestureManager : MonoBehaviour
 {
     public static GazeGestureManager Instance { get; private set; }
 
-    // Represents the hologram that is currently being gazed at.
-    public GameObject FocusedObject { get; private set; }
-
     private AudioSource audioSource;
 
     GestureRecognizer recognizer;
@@ -24,45 +21,7 @@ public class GazeGestureManager : MonoBehaviour
         recognizer.Tapped += (args) =>
         {
             audioSource.Play(); // play sound when user taps
-
-            // Send an OnSelect message to the focused object and its ancestors.
-            if (FocusedObject != null)
-            {
-                FocusedObject.SendMessageUpwards("OnSelect", SendMessageOptions.DontRequireReceiver);
-            }
         };
         recognizer.StartCapturingGestures();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Figure out which hologram is focused this frame.
-        GameObject oldFocusObject = FocusedObject;
-
-        // Do a raycast into the world based on the user's
-        // head position and orientation.
-        var headPosition = Camera.main.transform.position;
-        var gazeDirection = Camera.main.transform.forward;
-
-        RaycastHit hitInfo;
-        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
-        {
-            // If the raycast hit a hologram, use that as the focused object.
-            FocusedObject = hitInfo.collider.gameObject;
-        }
-        else
-        {
-            // If the raycast did not hit a hologram, clear the focused object.
-            FocusedObject = null;
-        }
-
-        // If the focused object changed this frame,
-        // start detecting fresh gestures again.
-        if (FocusedObject != oldFocusObject)
-        {
-            recognizer.CancelGestures();
-            recognizer.StartCapturingGestures();
-        }
     }
 }
--- MuseumMR/Assets/Scripts/ToggleableView.cs
+++ MuseumMR/Assets/Scripts/ToggleableView.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 
-public class ToggleableView : MonoBehaviour {
-
+public class ToggleableView : MonoBehaviour, IInputClickHandler, IInputHandler
+{
     public GameObject model1;
     public GameObject model2;
 
-    public void OnSelect()
+    public void OnInputClicked(InputClickedEventData eventData)
     {
         // toggle object views
         model1.SetActive(!model1.activeSelf);
         model2.SetActive(!model2.activeSelf);
     }
+
+    public void OnInputDown(InputEventData eventData)
+    { }
+
+    public void OnInputUp(InputEventData eventData)
+    { }
 }
