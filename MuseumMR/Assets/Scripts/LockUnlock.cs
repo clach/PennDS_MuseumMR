1681e3af1b5a5f682fffd6776a0d51e36a83aab3
clach <clach@seas.upenn.edu>
2018-11-05 13:23:07 -0500

    can lock/unlock model pos

---
+++ MuseumMR/Assets/Scripts/LockUnlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LockUnlock : MonoBehaviour {
+
+    public GameObject model;
+    public GameObject imageTarget;
+
+    private bool locked = false;
+
+    public void LockUnlockModel()
+    {
+        if (locked) {
+            model.transform.parent = imageTarget.transform;
+            GameObject.Find("LockUnlockButton").GetComponentInChildren<Text>().text = "Lock";
+            locked = false;
+        }
+        else
+        {
+            model.transform.parent = null;
+            GameObject.Find("LockUnlockButton").GetComponentInChildren<Text>().text = "Unlock";
+            locked = true;
+        }
+    }
+}
