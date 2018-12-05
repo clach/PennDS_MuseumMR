6786d01fec5a78d0fa5f28330413951ea6f3aaf1
clach <clach@seas.upenn.edu>
2018-12-05 13:35:48 -0500

    played with different model targets

---
+++ MuseumHoloLens/Assets/Scripts/ManipulationHandler.cs
@@ -0,0 +1,28 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class ManipulationHandler : MonoBehaviour, IManipulationHandler
+{
+    public GameObject model1;
+    public GameObject model2;
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
